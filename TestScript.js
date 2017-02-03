0ca2dc7bb3679bb42cfb8498086832f75acdd71e
KhaiRunner <khairunner@gmail.com>
2017-02-03 10:28:13 +0700

    Test load facebook first

--- TestScript.js
+++ TestScript.js
@@ -507,16 +507,25 @@ deferResizeToParent(function() {
 });
 }else{
 	//Content Page script
+	
+	(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+	
 	deferJquery(function() {
     handleImg();
     loadRelatedPosts();
-});
-deferJquery(function() {
-    $.ajaxSetup({
+	
+	 $.ajaxSetup({
         cache: true
     });
-	$(".fb-comments").attr("data-href", window.location.href);
-    $.getScript("https://apis.google.com/js/platform.js");
+    
+	$.getScript("https://apis.google.com/js/platform.js");
     (function(a, b, c) {
         var d = a.getElementsByTagName(b)[0];
         if (!a.getElementById(c)) {
@@ -529,6 +538,8 @@ deferJquery(function() {
     var windowWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
     stickyFB(windowWidth);
 });
+   
+
 }
 //----------------------------------------------------
 // All Page Last section
