cc4d77b13108b83f127608499646431b74eec529
KhaiRunner <khairunner@gmail.com>
2017-02-03 11:06:27 +0700

    using https

--- TestScript.js
+++ TestScript.js
@@ -7,7 +7,7 @@ if(isContentPage){
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+        js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 }
