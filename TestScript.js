0623a25c324e379374b5041b7a9eefe1a03dac57
Khai <khairunner@gmail.com>
2017-10-11 09:40:00 +0700

    log how long we need to wait jquery.

--- TestScript.js
+++ TestScript.js
@@ -19,7 +19,7 @@ function addWidgets(){
 }
 addWidgets();
 
-function deferJquery(a){window.jQuery?a():setTimeout(function(){deferJquery(a)},50)}
+function deferJquery(a){window.jQuery?a():setTimeout(function(){console.log('deferJquery');deferJquery(a)},50)}
 function deferResizeToParent(a){window.jQuery&&$.isFunction($.fn.resizeToParent)?a():setTimeout(function(){deferResizeToParent(a)},50)}
 deferJquery(function(){(function(e){e.fn.resizeToParent=function(t){function r(e){e.css({width:"",height:"","margin-left":"","margin-top":""});var n=e.parents(t.parent).width();var r=e.parents(t.parent).height();var i=e.width();var s=e.height();var o=i/n;if(s/o<r){e.css({width:"auto",height:r});i=i/(s/r);s=r}else{e.css({height:"auto",width:n});i=n;s=s/o}var u=(i-n)/-2;var a=(s-r)/-2;e.css({"margin-left":u,"margin-top":a})}var n={parent:"div",delay:100};var t=e.extend(n,t);var i;var s=this;e(window).on("resize",function(){clearTimeout(i);i=setTimeout(function(){s.each(function(){r(e(this))})},t.delay)});return this.each(function(){var t=e(this);t.attr("src",t.attr("src"));t.load(function(){r(t)});if(this.complete){r(t)}})}})(jQuery);});
 function updateLink(links){
