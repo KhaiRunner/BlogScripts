a0449624a2f5912db5d8515b3602c077b6ff8907
Khai Runner <Khairunner@gmail.com>
2018-11-08 09:37:10 +0700

    Optimize logic check mobile will use userAgent only.

--- TestScript.js
+++ TestScript.js
@@ -567,7 +567,7 @@ function findScriptSection() {
 //-----------------------------------------------------------------------
 //Run Script All page Last section
 if (1200 < windowWidth) {
-	-1 != window.location.href.indexOf("?m=1") || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || stickySidebar();
+	-1 != /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || stickySidebar();
 }
 $("#sBtn").click(function() {
 	$("#sBox").animate({
@@ -582,6 +582,7 @@ $(".del").click(function() {
 });
 $(".error_page #main-wrapper").prepend('<div class="error-title"><span>404</span>');
 
+//WidgetMangaer we need to wait blog widget.js that just email only.
 function deferWidgetManager(a){window._WidgetManager?a():setTimeout(function(){deferWidgetManager(a)},5)}
 deferWidgetManager(findScriptSection);
 
