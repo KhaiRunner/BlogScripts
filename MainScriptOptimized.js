4a18680fbdc6f9a1b13e0d362b7e021615c317e8
Khai Runner <Khairunner@gmail.com>
2019-03-12 14:34:51 +0700

    Update home page and recently posts

--- MainScriptOptimized.js
+++ MainScriptOptimized.js
@@ -95,7 +95,7 @@ function K() {
 
 function G(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://4.bp.blogspot.com/-z7vrztPVgpY/XIH0pDOeggI/AAAAAAABjN0/HxHbEiG1KS41TfnkoCen7ZO5iwjKUtMvwCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://2.bp.blogspot.com/-gUaCUE0vKik/XIdgnW5Qb1I/AAAAAAABjj4/lmCvqYkFqw88SKykMRmtqPXAULEblffYACLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
