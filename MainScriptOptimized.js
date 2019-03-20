188442588289dede8bf14b5e6b90739f0c1ad239
Khai Runner <Khairunner@gmail.com>
2019-03-20 12:55:08 +0700

    update home page and recently url

--- MainScriptOptimized.js
+++ MainScriptOptimized.js
@@ -10,7 +10,7 @@ var M = false;
 //recentlyImageUrl
 
 function D(){
-	var directInfo = "จุดชมวิวพัทยา นั่งจิบกาแฟ บรรยากาศชิวๆ,Promotion เทสโก้ โลตัส สำหรับวันที่ 14-20 มี.ค. 62,เลือกเตียงนอนแบบไหนกันดี มาดูกันดีกว่า,โปรเซเว่น & โบรชัวร์ประจำวันที่ 14 - 20 มีนาคม 2562,Space Inspirium ที่เที่ยวสนุกๆ ว่างๆไปเที่ยวได้,/2019/03/view-point-at-pattaya.html,/2019/03/promotion-tesco-lotus-14-20Mar.html,/2019/03/tips-choose-your-bed.html,/2019/03/7-11-promotion-14-20Mar.html,/2019/03/one-day-trip-Space-Inspirium.html,,,,,";
+	var directInfo = "แต่งบ้านสวยงาม น่าอยู่ด้วยโทนสีพาสเทล สีทาบ้านสวยมากๆ,ประดิษฐ์ของแต่งบ้านให้สีเสน่ห์ เก๋ เท่ห์เฉพาะตัว,สีทาบ้านสีเหลืองอมเขียว บ้านของเราจะโดดเด่นแค่ไหนมาดูกัน,แบบห้องครัวประหยัดๆ กับตัวอย่างมากกว่า 40 แบบ,Art In Paradise พัทยา กว้างมาก มุมถ่ายรูปเยอะ,/2019/03/pastel-color-home-interior-examples.html,/2019/03/DIY-ideas-decorate-items-for-your-home.html,/2019/03/yellow-green-home-designs.html,/2019/03/cheap-and-budget-kitchen-design-ideas.html,/2019/03/art-in-paradise-pattaya.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function K() {
 
 function G(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://2.bp.blogspot.com/-5TVJksTjRzc/XIsX5qw7SRI/AAAAAAABj9s/8pQjex4XpooDegzQzix_b9YezTmYEb7XACLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://2.bp.blogspot.com/-pK76e7cgNiE/XJHVFXBERpI/AAAAAAABkOc/8iz1__2uFh8ES4ejKuUTdwXYzCQUBvXeQCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
