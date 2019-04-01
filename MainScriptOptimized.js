abb3fb1ebcf7968323b873abaf675bae46034569
Khai Runner <Khairunner@gmail.com>
2019-04-01 15:03:05 +0700

    update home page

--- MainScriptOptimized.js
+++ MainScriptOptimized.js
@@ -10,7 +10,7 @@ var M = false;
 //recentlyImageUrl
 
 function D(){
-	var directInfo = "วิธีทำน้ำแข็งละลายช้า กินน้ำเย็นๆได้นานมาก,โปรโมชั่นห้างแม็คโคร ประจำวันที่ 27 มี.ค. - 9 เม.ย. 2562,ปลูกต้นไม้ในห้องนอน จัดสวนเก๋ๆกับกระถางสวยๆ,ตัวอย่างแต่งที่นั่งเล่นระเบียงหน้าบ้าน มาดูไอเดียกันเลย,Promotion Tesco Lotus 21-27 มีนาคม 2562,/2019/03/how-to-make-ice-melt-slowly.html,/2019/03/makro-check-price-27Mar-9Apr-2019.html,/2019/03/garden-in-your-bedroom-ideas.html,/2019/03/front-porch-design-ideas.html,/2019/03/promotion-tesco-lotus-21-27Mar2019.html,,,,,";
+	var directInfo = "ไอเดียแต่งระเบียงคอนโดสวยๆ ดูตัวอย่างเยอะเลย,ตัวอย่างแต่งห้องน้ำเล็กๆ ให้ดูกว้าง น่าใช้ แนวโมเดิร์น,ทาสีนอกบ้านสีน้ำตาล บ้านจะสวยไหม มาดูตัวอย่างกัน,ไอเดียทาสีผนังบ้านสวยงาม ดูดีอย่างมีสไตล์,ตัวอย่างแต่งห้องคอนโดเล็กๆให้น่าอยู่,/2019/04/beautiful-balcony-decoration-ideas.html,/2019/04/small-modern-bathroom-design.html,/2019/04/ideas-for-painting-house-in-brown-color.html,/2019/04/cool-ideas-for-painted-your-home.html,/2019/04/small-condo-interior-example.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function K() {
 
 function G(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://1.bp.blogspot.com/-5kTiY5l477o/XJrwoBSI-SI/AAAAAAABkdU/c2nGkl7VDfc6JFGmMzcPjjFztCmo-429gCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://4.bp.blogspot.com/-vl8j8inF7MI/XKHFPHPBFKI/AAAAAAABkvE/q-V5IXj4YashPq6W7lxd-KWhi2pb9VUQACLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
