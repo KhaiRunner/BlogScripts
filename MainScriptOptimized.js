7bb7f70eb7f81db33a59e46ff7ddbb9a13bcc8b4
Khai Runner <Khairunner@gmail.com>
2019-04-09 12:58:06 +0700

    update home page

--- MainScriptOptimized.js
+++ MainScriptOptimized.js
@@ -10,7 +10,7 @@ var M = false;
 //recentlyImageUrl
 
 function D(){
-	var directInfo = "แสตมป์โลตัสล่าสุด สามหมีจอมป่วน We bare bears.,โปรโมชั่นเทสโก้ โลตัส 4-10 เมษายน 2562,โปรโมชั่น 711 ประจำวันที่ 4-10 เดือนเมษายน 2562 พร้อม barcode ลดราคา,ไอเดียตกแต่งเสาบ้าน มาตกแต่งเสาบ้านเก๋ๆกันเลย,วิธีแก้แอร์ไม่เย็น ถอดแอร์มาล้างกันให้ดีๆก่อน,/2019/04/latest-stamp-lotus-2019-We-bare-bears.html,/2019/04/promotion-tesco-lotus-4-10April2019.html,/2019/04/711-promotion-4-10April.html,/2019/04/house-poles-decorate-ideas.html,/2019/04/fix-air-conditioner-is-not-cooling.html,,,,,";
+	var directInfo = "Aquarium ระยอง ราคาถูก บรรยากาศดี มาเที่ยวกันดีกว่า,ร้านอาหารตำนานป่า  บรรยากาศสุดชิวที่ระยอง ต้องมาให้ได้,กางเต้นท์ ณ เขาแหลมหญ้า ลูกสนุกมาก มีความสุขกันสุดๆ,นั่งเรือลอดถ้ำ เขาชะอางคร่อมคลอง ที่ระยอง สนุก ตื่นเต้นดี มาชมกันเลย,งานทิวลิปบานที่ระยอง 2562 มีรูปเยอะ ดอกไม้สวยมากๆ,/2019/04/review-Rayong-Aquarium.html,/2019/04/Tamnan-Pa-Restaurant.html,/2019/04/Khao-Laem-Ya-camping.html,/2019/04/Take-a-boat-through-the-cave.html,/2019/04/Blooming-Tulips-at-Rayong.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function K() {
 
 function G(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://4.bp.blogspot.com/-XuCuNry0ED8/XKV1W6iK_WI/AAAAAAABlEo/wySDbteWI1MQ9EvDO19h4Yps3lFrC1F8gCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://3.bp.blogspot.com/-K9j5_5PEtrI/XKwzyJZCNBI/AAAAAAABlVs/bCuPyV2roOku9-OEBPanUX9YCC73aHpBgCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
