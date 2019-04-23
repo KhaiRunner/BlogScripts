7b8bba0e23eb19443d09e1ac5eebf58ee4759a01
Khai Runner <Khairunner@gmail.com>
2019-04-23 12:59:18 +0700

    update home page

--- MainScript.js
+++ MainScript.js
@@ -10,7 +10,7 @@ var isInitFB = false;
 //recentlyImageUrl
 
 function getRecentPost(){
-	var directInfo = "ไอเดียบ้านไม้สวยๆ หลังเล็ก น่ารัก แต่งบ้านไม้น่าอยู่,ตัวอย่างแต่งห้องนอนบ้านไม้เก่าๆ มาดูกันเลย,ตกแต่งบันไดบ้านสวยๆ ดูดี มีสไตล์เฉพาะตัว,วิธีโกนหนวดให้สิวไม่ขึ้น ชีวิตจะได้ไม่น่าเบื่ออีก,เปรียบเทียบเครื่องฟอกอากาศ Xiaomi vs Sharp ใครดีกว่ากัน,/2019/04/cosy-small-wooden-house-designs.html,/2019/04/decorate-bedroom-in-wooden-house-ideas.html,/2019/04/decor-stair-ideas.html,/2019/04/How-to-shaving-and-prevent-acne.html,/2019/04/Air-purifier-Xiaomi-Pro-VS-Sharp.html,,,,,";
+	var directInfo = "แบบบ้านชั้นครึ่งโมเดิร์นมากๆ อารมณ์บ้านใต้ถุนสูง,วิธีจัดสวนแนวตั้ง + ตัวอย่าง ทำเองกันได้ มาดูไอเดียกัน,ตัวอย่างจัดสวนแนวตั้งสวยๆ ร่มรื่นสุดๆ บ้านน่าอยู่,แบบบ้านสไตล์วินเทจ บ้านสวยๆ บรรยากาศสบายๆ,แบบเพดานไม้สุดสวย แต่งบ้านให้ผ่อนคลาย น่าอยู่,/2019/04/modern-1.5-storey-house-design-ideas-with-interior-picture.html,/2019/04/how-to-create-a-vertical-garden.html,/2019/04/vertical-garden-example.html,/2019/04/cozy-house-design-with-vintage-style.html,/2019/04/Beautiful-wooden-ceiling.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function initWidgetManager() {
 
 function addWidgets(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://1.bp.blogspot.com/-TVR0hEPw6FQ/XL1pO43IOPI/AAAAAAABl90/ZkNFWclI9T4N6gtN5QhU9Q4D7MzXH0-mgCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://3.bp.blogspot.com/-Y4qUk-nI6NA/XL6pIcsN0jI/AAAAAAABmPw/NFsXQowGBUAbY8pV_0_JakEKn62phLPfwCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
