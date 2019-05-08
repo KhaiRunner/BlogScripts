e128aca06a10bfeadc7079ae8b085ab2b472c2bf
Khai Runner <Khairunner@gmail.com>
2019-05-08 07:01:17 +0700

    update home page

--- MainScriptOptimized.js
+++ MainScriptOptimized.js
@@ -10,7 +10,7 @@ var M = false;
 //recentlyImageUrl
 
 function D(){
-	var directInfo = "ไอเดียแบบบ้านโมเดิร์นสวยๆ โดดเด่นน่าสนใจ มาดูกันเลย,ไอเดียตกแต่งภายในบ้านสวยๆ เอาให้โมเดิร์นสุดๆ,สไตล์การแต่งบ้านโมเดิร์นสวยๆ แต่บรรยากาศต่างกัน ไปชมกันเลย,43 ไอเดียแต่งครัวที่บ้านให้สวยปิ๊ง โมเดิร์นน่าใช้มากมาย,รวมแบบห้องน้ำสวยๆ โมเดิร์นๆ ผ่อนคลาย น่าใช้งาน,/2019/04/modern-house-design-ideas.html,/2019/04/modern-interior-ideas-for-your-cozy-and-relax.html,/2019/04/interior-design-styles.html,/2019/04/43-beautiful-and-modern-kitchen-designs.html,/2019/04/modern-bathroom-designs.html,,,,,";
+	var directInfo = "ไอเดียแต่งคอนโดสวยๆ โมเดิร์น ห้องดูกว้าง น่าอยู่,โปรโมชั่น / โบรชัวร์ห้าง Makro ล่าสุด 8-21 พ.ค. 62,ไอเดียแบบบ้านโมเดิร์นสวยๆ โดดเด่นน่าสนใจ มาดูกันเลย,ไอเดียตกแต่งภายในบ้านสวยๆ เอาให้โมเดิร์นสุดๆ,สไตล์การแต่งบ้านโมเดิร์นสวยๆ แต่บรรยากาศต่างกัน ไปชมกันเลย,/2019/05/cozy-modern-condo-interior-ideas.html,/2019/05/promotion-makro-8-21May2019.html,/2019/04/modern-house-design-ideas.html,/2019/04/modern-interior-ideas-for-your-cozy-and-relax.html,/2019/04/interior-design-styles.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function K() {
 
 function G(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://1.bp.blogspot.com/-f7-zVAFjzQI/XMaDiMUKW6I/AAAAAAABmzw/4pwta5vsnGUhAU4gnaFuONBDxocROWrJQCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://4.bp.blogspot.com/-hAmC7_vtukw/XNIbpmW7-II/AAAAAAABm3A/KZJqXeWXVXswtq9pTTodywoeAAmQrBzeQCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
