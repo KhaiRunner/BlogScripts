dba96cfb1edc78425cf86925d718ba0e2d214a78
Khai Runner <Khairunner@gmail.com>
2019-05-16 13:17:31 +0700

    update testing script

--- TestScript.js
+++ TestScript.js
@@ -95,7 +95,7 @@ function initWidgetManager() {
 
 function addWidgets(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://1.bp.blogspot.com/-bvCwrksEA88/XIn0uzU68VI/AAAAAAABj6I/d1fnejYJlBACjW3bwEHSBXulhkizgRg-wCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://3.bp.blogspot.com/-EKLAONHhZ-Y/XNu-gUd4NeI/AAAAAAABnm8/xgqjrLODRo0W_O05GmNtV5oONnsgzncvwCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
@@ -291,8 +291,7 @@ if(isContentPage){
 	handleImg();
 	
 	//Add Meta tags for content pages.
-	var firstImageUrl = $('.pb img').first().attr('src');
-	$('head').append("<meta content='article' property='og:type'/><meta content='"+ firstImageUrl +"' property='og:image'/>");	
+	$('head').append("<meta content='article' property='og:type'/>");	
 	
 	
 	stickyFB(windowWidth);
