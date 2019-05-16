9bef6825dd333a6d57387ecd0c2cde38052fef15
Khai Runner <Khairunner@gmail.com>
2019-05-16 13:25:38 +0700

    update home page

--- MainScript.js
+++ MainScript.js
@@ -10,7 +10,7 @@ var isInitFB = false;
 //recentlyImageUrl
 
 function getRecentPost(){
-	var directInfo = "ฟินเหมือนไปเมืองนอก Mega Park พาลูกวิ่งเล่น ปั่นจักรยานกันสนุกมากมาย,ที่เที่ยวฟรีในหัวหิน วัดห้วยมงคล ไปเที่ยวกันดีกว่า,อุทยานสิ่งแวดล้อมนานาชาติสิรินธร ที่เที่ยวฟรีๆในหัวหิน,เที่ยวฟรี ดี@ชะอำ-หัวหิน เขาพันธุรัต ไปชมวิวกัน,รีวิว AKA Resort & Spa หัวหินล่าสุด สะอาด-คุ้มค่าไหม? มาดูกันเลย,/2019/05/Mega-Park-good-park-for-kids-and-family.html,/2019/05/Wat-Huay-Mongkol.html,/2019/05/Sirindhorn-International-Environmental-Park.html,/2019/05/Khao-Nang-Panthurat-Forest-Park.html,/2019/05/review-AKA-Resort-Spa-Hua-Hin.html,,,,,";
+	var directInfo = "101 แบบห้องน้ำสวยๆ โมเดิร์น น่าใช้งาน,ไอเดียแต่งห้องนั่งเล่นประหยัดๆ สวยงาม อารมณ์สบายๆ,102 แบบห้องครัวสวยๆ ตามหาห้องครัวโมเดิร์นที่โดนใจคุณ,แบบห้องนั่งเล่นสีส้ม สีเขียว สีแดง สีน้ำตาล ชอบสีไหน?!,แบบห้องครัวเปิดโล่ง สวย หรู ดูมีระดับ,/2019/05/101-modern-bathroom-designs.html,/2019/05/save-money-interior-living-room-designs.html,/2019/05/102-modern-cozy-kitchen-designs.html,/2019/05/modern-living-room-colors.html,/2019/05/open-space-kitchen-design.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function initWidgetManager() {
 
 function addWidgets(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://3.bp.blogspot.com/-EKLAONHhZ-Y/XNu-gUd4NeI/AAAAAAABnm8/xgqjrLODRo0W_O05GmNtV5oONnsgzncvwCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://4.bp.blogspot.com/-SFVNm-LfTRM/XN0B4k6KDrI/AAAAAAABn_s/i4v-a-ES2pUTCGEsPXvQfH6Wi96O1lCKQCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
--- MainScriptOptimized.js
+++ MainScriptOptimized.js
@@ -10,7 +10,7 @@ var M = false;
 //recentlyImageUrl
 
 function D(){
-	var directInfo = "ฟินเหมือนไปเมืองนอก Mega Park พาลูกวิ่งเล่น ปั่นจักรยานกันสนุกมากมาย,ที่เที่ยวฟรีในหัวหิน วัดห้วยมงคล ไปเที่ยวกันดีกว่า,อุทยานสิ่งแวดล้อมนานาชาติสิรินธร ที่เที่ยวฟรีๆในหัวหิน,เที่ยวฟรี ดี@ชะอำ-หัวหิน เขาพันธุรัต ไปชมวิวกัน,รีวิว AKA Resort & Spa หัวหินล่าสุด สะอาด-คุ้มค่าไหม? มาดูกันเลย,/2019/05/Mega-Park-good-park-for-kids-and-family.html,/2019/05/Wat-Huay-Mongkol.html,/2019/05/Sirindhorn-International-Environmental-Park.html,/2019/05/Khao-Nang-Panthurat-Forest-Park.html,/2019/05/review-AKA-Resort-Spa-Hua-Hin.html,,,,,";
+	var directInfo = "101 แบบห้องน้ำสวยๆ โมเดิร์น น่าใช้งาน,ไอเดียแต่งห้องนั่งเล่นประหยัดๆ สวยงาม อารมณ์สบายๆ,102 แบบห้องครัวสวยๆ ตามหาห้องครัวโมเดิร์นที่โดนใจคุณ,แบบห้องนั่งเล่นสีส้ม สีเขียว สีแดง สีน้ำตาล ชอบสีไหน?!,แบบห้องครัวเปิดโล่ง สวย หรู ดูมีระดับ,/2019/05/101-modern-bathroom-designs.html,/2019/05/save-money-interior-living-room-designs.html,/2019/05/102-modern-cozy-kitchen-designs.html,/2019/05/modern-living-room-colors.html,/2019/05/open-space-kitchen-design.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function K() {
 
 function G(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://3.bp.blogspot.com/-EKLAONHhZ-Y/XNu-gUd4NeI/AAAAAAABnm8/xgqjrLODRo0W_O05GmNtV5oONnsgzncvwCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://4.bp.blogspot.com/-SFVNm-LfTRM/XN0B4k6KDrI/AAAAAAABn_s/i4v-a-ES2pUTCGEsPXvQfH6Wi96O1lCKQCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
@@ -362,9 +362,6 @@ function J() {
         topPos = mainWrapper.offset().top,
 		endOfContentPosition = A ? $('.pf').offset().top : 0,
         html3 = $("#HTML3"),
-        html3Height = html3.height(),
-        scrollLength = topPos - html3Height,
-        stopPos = mainWrapper.height() + scrollLength,
         startPos = topPos + 459;
     $(window).scroll(function() {
         var scroll = $(this).scrollTop();
@@ -378,6 +375,8 @@ function J() {
 			$(".so").append('<div style=padding-top:9px>&#8193;📣 ช่วยแชร์หน่อยนะ 😙🙏</div>');
 		}
 		
+		var html3Height = html3.height();
+        var scrollLength = topPos - html3Height;
         stopPos = mainWrapper.height() + scrollLength;
         scroll < startPos ? html3.css({
             position: "relative"
