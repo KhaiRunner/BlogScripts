a5b8356ae9266efc0bda6674c0bbd09824577668
Khai Runner <Khairunner@gmail.com>
2019-05-16 15:48:56 +0700

    update home page

--- MainScript.js
+++ MainScript.js
@@ -10,7 +10,7 @@ var isInitFB = false;
 //recentlyImageUrl
 
 function getRecentPost(){
-	var directInfo = "101 แบบห้องน้ำสวยๆ โมเดิร์น น่าใช้งาน,ไอเดียแต่งห้องนั่งเล่นประหยัดๆ สวยงาม อารมณ์สบายๆ,102 แบบห้องครัวสวยๆ ตามหาห้องครัวโมเดิร์นที่โดนใจคุณ,แบบห้องนั่งเล่นสีส้ม สีเขียว สีแดง สีน้ำตาล ชอบสีไหน?!,แบบห้องครัวเปิดโล่ง สวย หรู ดูมีระดับ,/2019/05/101-modern-bathroom-designs.html,/2019/05/save-money-interior-living-room-designs.html,/2019/05/102-modern-cozy-kitchen-designs.html,/2019/05/modern-living-room-colors.html,/2019/05/open-space-kitchen-design.html,,,,,";
+	var directInfo = "104 แบบห้องนั่งเล่นสวยๆ โมเดิร์นมากๆ แต่งบ้านน่าอยู่จริงๆ,แต่งบ้านสไตล์วินเทจ หรูหรามาก น่าอยู่จัง,101 แบบห้องน้ำสวยๆ โมเดิร์น น่าใช้งาน,ไอเดียแต่งห้องนั่งเล่นประหยัดๆ สวยงาม อารมณ์สบายๆ,102 แบบห้องครัวสวยๆ ตามหาห้องครัวโมเดิร์นที่โดนใจคุณ,/2019/05/104-cozy-modern-living-room-designs.html,/2019/05/luxury-vintage-interior-idea.html,/2019/05/101-modern-bathroom-designs.html,/2019/05/save-money-interior-living-room-designs.html,/2019/05/102-modern-cozy-kitchen-designs.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function initWidgetManager() {
 
 function addWidgets(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://4.bp.blogspot.com/-SFVNm-LfTRM/XN0B4k6KDrI/AAAAAAABn_s/i4v-a-ES2pUTCGEsPXvQfH6Wi96O1lCKQCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://2.bp.blogspot.com/-938nR_yXUTY/XN0ja_kml1I/AAAAAAABoIU/GE0O__UsTCMnX66h1WHzmss5SO798fISACLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
