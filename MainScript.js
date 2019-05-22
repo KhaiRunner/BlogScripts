e51dbab0a2fa818725a50b8ecff4b35dd803ab21
Khai Runner <Khairunner@gmail.com>
2019-05-22 14:56:51 +0700

    update home page

--- MainScript.js
+++ MainScript.js
@@ -10,7 +10,7 @@ var isInitFB = false;
 //recentlyImageUrl
 
 function getRecentPost(){
-	var directInfo = "107 ไอเดียห้องครัวราคาประหยัด แบบครัวบ้านๆถูกๆ,ตัวอย่างปูสนามหญ้าทั้งหน้าบ้านและหลังบ้าน มาดูกันเลย,57 แบบจัดสวนมุมบ้านสวยๆ หาไอเดียจัดสวนที่บ้านกัน,แบบสวนหน้าเรียบง่าย พร้อมสนามหญ้าหลังบ้านสวยๆ,107 ไอเดียแต่งห้องรับประทานที่บ้านให้สวย น่านั่ง บรรยากาศน่าอยู่,/2019/05/107-budget-kitchen-designs.html,/2019/05/frontyard-and-backyard-design-for-your-home.html,/2019/05/57-beautiful-garden-corner-designs.html,/2019/05/frontyard-and-backyard-design-example.html,/2019/05/107-cozy-and-beautiful-dining-room-designs.html,,,,,";
+	var directInfo = "ไอเดียแต่งระเบียงคอนโด มาดูตัวอย่างในชีวิตจริงกัน,ไม้จิ้มฟันนั้นไซร้ คือ ของแต่งบ้านสุดอลังการ!!!,แต่งบ้านโมเดิร์น บรรยากาศสดใส ดูดี น่าอยู่,70 ไอเดียแต่งที่นั่งเล่นระเบียงหน้าบ้าน ดูตัวอย่างกัน,18 ตัวอย่างติดผ้าม่านตาไก่ จะสวยไหม มาดูกันเลย,/2019/05/balcony-condo-decoration-ideas.html,/2019/05/DIY-decorate-items-from-Toothpick.html,/2019/05/modern-interior-home-example.html,/2019/05/70-decor-ideas-for-front-porch.html,/2019/05/eyelet-curtains-ideas.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function initWidgetManager() {
 
 function addWidgets(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://2.bp.blogspot.com/-z9QGpE_31bo/XONyF5fBMII/AAAAAAABo2I/Q3DRL3ku5Z0ywixi8LUyH7Po5Ymb9PTJACLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://3.bp.blogspot.com/-iw1_JTezCbg/XOUAUUJyNZI/AAAAAAABpCM/RqmnkPNdUqYjeFMASsRCWugZpHlHXDlvwCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
