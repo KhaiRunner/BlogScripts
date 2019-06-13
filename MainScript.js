f76cad6b9478a1e432984590580d5f3373d7e7b2
Khai Runner <Khairunner@gmail.com>
2019-06-13 13:30:56 +0700

    update home page

--- MainScript.js
+++ MainScript.js
@@ -10,7 +10,7 @@ var isInitFB = false;
 //recentlyImageUrl
 
 function getRecentPost(){
-	var directInfo = "แบบเตียง 2 ชั้นสวยๆ ต่างรูปทรง ตอบโจทย์ความต้องการ,DIY ที่แขวนหมวกเก๋ๆ ที่แขวนของสวยๆ มาประดิษฐ์กัน,ไอเดียตู้เก็บของในห้องน้ำสวยๆ แต่งห้องน้ำให้ดูดี มีระดับ,ไอเดียแต่งห้องนอนเด็กสวยๆ น่ารัก น่าอยู่มากมาย,D.I.Y. ช่อดอกไม้สด แต่งบ้านสวยๆ ติดผนังงามๆ,/2019/06/cozy-bunk-bed-designs-for-kids-bedroom.html,/2019/06/DIY-beautiful-hanger-shelf.html,/2019/06/beautiful-cabinet-for-bathroom-designs.html,/2019/06/cozy-kids-bedroom-designs.html,/2019/05/DIY-rose-heart-decorate-your-home.html,,,,,";
+	var directInfo = "720 แบบห้องนอน แต่งห้องนอนสวยๆให้โดนใจ,ประดิษฐ์ของแต่งบ้านง่ายๆ ที่แขวนของสวยๆ ด้วยวิชาลูกเสือ!!!,DIY โคมไฟแต่งบ้านสวยๆ แนวลอฟท์ เก๋ เท่ห์ ประหยัด,โปรโมชั่นห้างแม็คโคร / เช็คราคาสินค้า 5-18 มิถุนายน 2562,แบบเตียง 2 ชั้นสวยๆ ต่างรูปทรง ตอบโจทย์ความต้องการ,/2019/06/720-modern-cozy-bedroom-designs.html,/2019/06/DIY-simple-home-decoration.html,/2019/06/DIY-lamp-in-loft-style.html,/2019/06/Makro-promotion-5-18Jun.html,/2019/06/cozy-bunk-bed-designs-for-kids-bedroom.html,,,,,";
 	
 	//rawRecentPosts -> r
 	var infoElement = document.getElementById("r");
@@ -95,7 +95,7 @@ function initWidgetManager() {
 
 function addWidgets(){
 	//i -> image url.
-	var recentlyImageUrl = 'https://1.bp.blogspot.com/-iR76JyftPRY/XPYTxMr-4kI/AAAAAAABrcc/iyOYbtchCcEgbpgTxEhCQVPf3-ndwj4FQCLcBGAs/s1600/r.png';
+	var recentlyImageUrl = 'https://1.bp.blogspot.com/-dYBZzEGgA-Y/XQHtGWvz8NI/AAAAAAABsR4/hPKfpdNWaY0zYlfihnyLY-skp9AiEnJyQCLcBGAs/s1600/r.png';
 	var recentlyInfo = '.p1,.p2,.p3,.p4,.p5,.p6,.p7,.p8,.p9,.p10,.p11,.p12,.p13,.p14{background:url('+ recentlyImageUrl +');width:72px;height:72px}';
 	
 	var htmlWeeklyPop = '<style>' + recentlyInfo + '.sb .wc{padding:9px}.sb h2{background-color:#7A5;color:#FFF;display:inline-block;font-size:13px;padding:8px}.ll a{display:block;padding:0 0 12px}.cl a{float:left;background-color:#222;padding:5px 9px;margin:4px}.gp{height:72px}.pp li{list-style:none;display:inline-block}.pp .p{float:left;margin:0 5px 10px 0}.pp .t{line-height:21px}.i b{font-size:17px}.p1{background-position:0 0}.p2{background-position:0 -72px}.p3{background-position:0 -144px}.p4{background-position:0 -216px}.p5{background-position:0 -288px}.p6{background-position:0 -360px}.p7{background-position:0 -432px}.p8{background-position:0 -504px}.p9{background-position:0 -576px}.p10{background-position:0 -648px}.p11{background-position:0 -720px}.p12{background-position:0 -792px}.p13{background-position:0 -864px}.p14{background-position:0 -936px}</style>'
